5ffe6e1343acf31cddcbc79bf1b5feb0cde76487
DebabrataBanik <debabratabanik02@gmail.com>
2024-11-15 20:33:42 +0530

    add modal animation

--- contact_form_using_react/src/components/Confirmation.jsx
+++ contact_form_using_react/src/components/Confirmation.jsx
@@ -1,8 +1,21 @@
 import check from '../assets/images/icon-success-check.svg'
+import { useState, useEffect } from 'react';
 
-function Confirmation() {
+function Confirmation({
+  isValid
+}) {
+  const [isDisplayed, setIsDisplayed] = useState(false);
+
+  useEffect(() => {
+    if (isValid) {
+      setIsDisplayed(true);
+    } else {
+      setIsDisplayed(false);
+    }
+  }, [isValid]);
+
   return (
-    <div className="confirmation">
+    <div className={`confirmation ${isDisplayed ? 'display' : ''}`}>
       <section className="title">
         <h2>Message Sent!</h2>
         <img src={check} alt="success icon" />
